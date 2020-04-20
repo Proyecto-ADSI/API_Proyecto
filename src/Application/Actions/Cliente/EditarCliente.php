2b19baa612f08d190e850084702aca5113a0e70c
Alexander Vergara <alexx4008@gmail.com>
2020-04-19 21:17:45 -0500

    Editar clientes con validación para agregar

--- src/Application/Actions/Cliente/EditarCliente.php
+++ src/Application/Actions/Cliente/EditarCliente.php
@@ -16,90 +16,107 @@ class EditarCliente extends ClienteAction {
     {
         $campos = $this->getFormData();
 
-        // Validar si se edita el plan corporativo
+        // Crear datos básicos líneas.
+        $DBL = new DBL(
+            $campos->Id_DBL,
+            $campos->Id_Cliente,
+            $campos->Id_Operador,
+            NULL,
+            $campos->Cantidad_Lineas,
+            $campos->Valor_Mensual,
+            $campos->Id_Calificacion_Operador,
+            $campos->Razones,
+            NULL
+        );
 
+        $Id_Plan_Corporativo = (int) $campos->Id_Plan_Corporativo;
+        $Id_Documentos = (int) $campos->Id_Documentos;
+
+        // Validar si se edita o registra el plan corporativo
         if ($campos->Validacion_PLan_C) {
 
-            // Validar si se editan los documentos soporte
-            if ($campos->Validacion_Doc_S) {
+            // Crear plan corporativo
+            $Plan_Corporativo = new Plan_Corporativo(
+                NULL,
+                NULL,
+                $campos->Fecha_Inicio,
+                $campos->Fecha_Fin,
+                $campos->Clausula,
+                $campos->Descripcion,
+                NULL
+            );
 
-                // Editar documentos
+            
+           
+            // Validar si se editan o registran los documentos soporte
+            if ($campos->Validacion_Doc_S) {
+                
 
                 $Doc_Soporte = new Doc_Soporte(
-                    $campos->Id_Documentos,
+                    NULL,
                     $campos->Camara_Comercio,
                     $campos->Cedula_RL,
                     $campos->Soporte_Ingresos,
                     $campos->Detalles_Plan_Corporativo
                 );
 
-                $this->Doc_SoporteRepository->EditarDocSoporte($Doc_Soporte);
+               
+                if($Id_Documentos > 0){
+                    // Editar documentos
+                    $Doc_Soporte->__set("Id_Documentos",$Id_Documentos);
+                    $Plan_Corporativo->__set("Id_Documentos",$Id_Documentos);
+                    $this->Doc_SoporteRepository->EditarDocSoporte($Doc_Soporte);
+                    
+                }else{
 
-                // Editar plan corporativo
-
-                $Plan_Corporativo = new Plan_Corporativo(
-                    $campos->Id_Plan_Corporativo,
-                    $campos->Id_Documentos,
-                    $campos->Fecha_Inicio,
-                    $campos->Fecha_Fin,
-                    $campos->Clausula,
-                    $campos->Descripcion,
-                    NULL
-                );
-
-                $this->Plan_CorporativoRepository->EditarPlan_Corporativo($Plan_Corporativo);
+                    // Registrar ducumentos
+                    $this->Doc_SoporteRepository->RegistrarDocSoporte($Doc_Soporte);
+                    $InfoIdDoc = $this->Doc_SoporteRepository->ConsultarUltimoRegistrado();
+                    $Id_Documentos = (int) $InfoIdDoc['Id_Documentos'];
+                    $Plan_Corporativo->__set("Id_Documentos",$Id_Documentos);
+                }
+                
+                if($Id_Plan_Corporativo > 0){
+                    // Editar plan corporativo con documentos.
+                    $Plan_Corporativo->__set("Id_Plan_Corporativo",$Id_Plan_Corporativo);
+                    $this->Plan_CorporativoRepository->EditarPlan_Corporativo($Plan_Corporativo);
+                    
+                }else{
+                    // Registrar plan corporativo con documentos.
+                    $this->Plan_CorporativoRepository->RegistrarPlan_Corporativo($Plan_Corporativo);
+                    $InfoIdPlan = $this->Plan_CorporativoRepository->ConsultarUltimoRegistrado();
+                    $Id_Plan_Corporativo = (int) $InfoIdPlan['Id_Plan_Corporativo'];
+                }
+                
             } else {
 
-                // Editar Plan Corporativo sin editar documentos
-                $Plan_Corporativo = new Plan_Corporativo(
-                    $campos->Id_Plan_Corporativo,
-                    null,
-                    $campos->Fecha_Inicio,
-                    $campos->Fecha_Fin,
-                    $campos->Clausula,
-                    $campos->Descripcion,
-                    NULL
-                );
-
-                $this->Plan_CorporativoRepository->EditarPlan_Corporativo($Plan_Corporativo);
+                if($Id_Plan_Corporativo > 0){
+                    // Editar plan corporativo sin documentos.
+                    $Plan_Corporativo->__set("Id_Plan_Corporativo",$Id_Plan_Corporativo);
+                    $this->Plan_CorporativoRepository->EditarPlan_Corporativo($Plan_Corporativo);
+                    $this->Doc_SoporteRepository->EliminarDocSoporte($Id_Documentos);
+                }else{
+                    // Registrar plan corporativo sin documentos.
+                    $this->Plan_CorporativoRepository->RegistrarPlan_Corporativo($Plan_Corporativo);
+                    $InfoIdPlan = $this->Plan_CorporativoRepository->ConsultarUltimoRegistrado();
+                    $Id_Plan_Corporativo = (int) $InfoIdPlan['Id_Plan_Corporativo'];
+                }
             }
 
             // Editar Datos básicos líneas con plan corporativo
+            $DBL->__set("Id_Plan_Corporativo",$Id_Plan_Corporativo);
+            $this->DBLRepository->EditarDBL($DBL);
 
-            $DBL = new DBL(
-                $campos->Id_DBL,
-                $campos->Id_Cliente,
-                $campos->Id_Operador,
-                $campos->Id_Plan_Corporativo,
-                $campos->Cantidad_Lineas,
-                $campos->Valor_Mensual,
-                $campos->Id_Calificacion_Operador,
-                $campos->Razones,
-                NULL
-            );
-
-           $this->DBLRepository->EditarDBL($DBL);
 
         } else {
 
             // Editar Datos básicos líneas sin plan corporativo
-            $DBL = new DBL(
-                $campos->Id_DBL,
-                $campos->Id_Cliente,
-                $campos->Id_Operador,
-                NULL,
-                $campos->Cantidad_Lineas,
-                $campos->Valor_Mensual,
-                $campos->Id_Calificacion_Operador,
-                $campos->Razones,
-                NULL
-            );
-
             $this->DBLRepository->EditarDBL($DBL);
+            $this->Plan_CorporativoRepository->EliminarPlan_Corporativo($Id_Plan_Corporativo);
+            $this->Doc_SoporteRepository->EliminarDocSoporte($Id_Documentos);
         }
 
         $arrayLineas = $campos->DetalleLineas;
-        $validacion = NULL;
 
         foreach($arrayLineas as $lineaItem){
 
@@ -124,19 +141,14 @@ class EditarCliente extends ClienteAction {
             }
 
             if($lineaItem->id > 0){
-
+                 // Editar
                 $linea->__set("Id_Linea",$lineaItem->id);
-
-                // Editar
-             $validacion = $this->LineaRepository->EditarLinea($linea);
-
-              
+                $this->LineaRepository->EditarLinea($linea);
             }else{
-
                 // Agregar
                 $this->LineaRepository->RegistrarLinea($linea);
                 $infoIdLinea = $this->LineaRepository->ConsultarUltimaLinea();
-                $validacion =  $this->LineaRepository->RegistrarDetalleLinea((int)$infoIdLinea['Id_Linea'], $campos->Id_DBL);
+                $this->LineaRepository->RegistrarDetalleLinea((int)$infoIdLinea['Id_Linea'], $campos->Id_DBL);
             }
         }
 
@@ -153,8 +165,6 @@ class EditarCliente extends ClienteAction {
             NULL
         );
         
-        // return $this->respondWithData(["ok" => $validacion]);
-
         $respuesta = $this->ClienteRepository->EditarCliente($Cliente);
 
         // Respuesta es TRUE || FALSE
