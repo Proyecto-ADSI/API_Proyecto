d738f95f24174bc39071b852cafcad327fe2dcf7
Alexander Vergara <alexx4008@gmail.com>
2020-06-27 17:39:38 -0500

    Test documentos

--- src/Application/Actions/Documento/CambiarEstadoAction.php
+++ src/Application/Actions/Documento/CambiarEstadoAction.php
@@ -10,14 +10,34 @@ class CambiarEstadoAction extends DocumentoAction
 {
     protected function action(): Response
     {
-        $Id_Documentos = $this->resolveArg("Id_Documentos");
+        $Id_Documento = (int) $this->resolveArg("Id_Documentos");
         $Estado = $this->resolveArg("Estado");
 
-        $datos = $this->DocumentoRepository->CambiarEstado(intval($Id_Documentos), intval($Estado));
-
-
-        return $this->respondWithData(["ok"=>$datos]);
-        
+        if ($Id_Documento == 0) {
+            $data = [
+                "statusCode:" => 400,
+                "type error:" => "BAD_REQUEST",
+                "Description" => "El ID no es valido"
+            ];
+            $payload = json_encode($data);
+            $this->response->getBody()->write($payload);
+            return $this->response
+                ->withHeader('Content-Type', 'application/json')
+                ->withStatus(400);
+        } else if (!is_numeric($Estado)) {
+            $data = [
+                "statusCode:" => 400,
+                "type error:" => "BAD_REQUEST",
+                "Description" => "El estado no es valido"
+            ];
+            $payload = json_encode($data);
+            $this->response->getBody()->write($payload);
+            return $this->response
+                ->withHeader('Content-Type', 'application/json')
+                ->withStatus(400);
+        } else {
+            $datos = $this->DocumentoRepository->CambiarEstado($Id_Documento, (int) $Estado);
+            return $this->respondWithData(["ok" => $datos]);
+        }
     }
 }
-
--- src/Application/Actions/Documento/EliminarDocumentoAction.php
+++ src/Application/Actions/Documento/EliminarDocumentoAction.php
@@ -4,27 +4,36 @@ declare(strict_types=1);
 
 namespace App\Application\Actions\Documento;
 
-use App\Domain\Documento\Documento;
 use Psr\Http\Message\ResponseInterface as Response;
 
 class EliminarDocumentoAction extends DocumentoAction
 {
     protected function action(): Response
     {
-        $Id_Documentos = (int)$this->resolveArg("Id_Documentos");
+        $Id_Documento = (int) $this->resolveArg("Id_Documentos");
+        if ($Id_Documento == 0) {
+            $data = [
+                "statusCode:" => 400,
+                "type error:" => "BAD_REQUEST",
+                "Description" => "El ID no es valido"
+            ];
+            $payload = json_encode($data);
+            $this->response->getBody()->write($payload);
+            return $this->response
+                ->withHeader('Content-Type', 'application/json')
+                ->withStatus(400);
+        } else {
+            $Validar = $this->DocumentoRepository->ValidarEliminarDocumento($Id_Documento);
 
-        $Validar = $this->DocumentoRepository->ValidarEliminarDocumento($Id_Documentos);
+            if (!empty($Validar)) {
 
-        if(!empty($Validar)){
+                return $this->respondWithData(["Eliminar" => false]);
+            } else {
 
-            return $this->respondWithData(["Eliminar" => false]);
-        }else{
+                $Eliminar = $this->DocumentoRepository->EliminarDocumento($Id_Documento);
 
-           $Eliminar = $this->DocumentoRepository->EliminarDocumento($Id_Documentos);
-
-            return $this->respondWithData(["Eliminar" => $Eliminar]);
+                return $this->respondWithData(["Eliminar" => $Eliminar]);
+            }
         }
-        
     }
 }
-
