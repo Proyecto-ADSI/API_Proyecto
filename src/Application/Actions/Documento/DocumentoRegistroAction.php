1d3af4c4aeab8ed23bc0a14b558b2e50ca9b049c
Alexander Vergara <alexx4008@gmail.com>
2020-06-26 19:39:54 -0500

    Ejemplo pruebas

--- src/Application/Actions/Documento/DocumentoRegistroAction.php
+++ src/Application/Actions/Documento/DocumentoRegistroAction.php
@@ -22,59 +22,55 @@ class DocumentoRegistroAction extends DocumentoAction
         $Estado = $campos->Estado;
 
         if (empty($Nombre)) {
-            # code...
             return $this->respondWithData([
                 "status code:" => 500,
                 "type error:" => "Server_error",
                 "Description" => "El nombre está vacío"
             ]);
-        }
-        else if(!is_string($Nombre)){
-            return $this->respondWithData([
-                "status code:" => 500,
+        } else if (!is_string($Nombre)) {
+            $data = [
+                "statusCode:" => 500,
                 "type error:" => "Server_error",
                 "Description" => "No es una cadena"
-            ]);
-        }
-        else if (strlen($Nombre)>45) {
-            # code...
+            ];
+            $payload = json_encode($data);
+            $this->response->getBody()->write($payload);
+            return $this->response
+                ->withHeader('Content-Type', 'application/json')
+                ->withStatus(500);
+        } else if (strlen($Nombre) > 45) {
             return $this->respondWithData([
-                "status code:" => 500,
                 "type error:" => "Server_error",
                 "Description" => "Superó el límite de carácteres"
             ]);
-        }
-        else if (strlen($Estado2)>11){
+        } else if (strlen($Estado2) > 11) {
             return $this->respondWithData([
                 "status code:" => 500,
                 "type error:" => "Server_error",
                 "Description" => "Superó el límite de carácteres numéricos"
             ]);
-        }
-        else if (is_null($Estado)){
+        } else if (is_null($Estado)) {
             return $this->respondWithData([
                 "status code:" => 500,
                 "type error:" => "Server_error",
                 "Description" => "El campo está nulo"
             ]);
-        }
-        else if(!is_numeric($Estado)){
+        } else if (!is_numeric($Estado)) {
             return $this->respondWithData([
                 "status code:" => 500,
                 "type error:" => "Server_error",
                 "Description" => "No es un numéro"
             ]);
-        }
-        else if(strlen($Nombre)>=1  && strlen($Nombre)<=45 && strlen($Estado2)>=1 && strlen($Estado2) <=11 )
+        } else if (strlen($Nombre) >= 1  && strlen($Nombre) <= 45 && strlen($Estado2) >= 1 && strlen($Estado2) <= 11)
 
-        $datos = new Documento(
-            0,
-            $Nombre,
-            $Estado
-        );
+            $datos = new Documento(
+                0,
+                $Nombre,
+                $Estado
+            );
 
         $datos = $this->DocumentoRepository->RegistrarDocumento($datos);
 
-        return $this->respondWithData(["ok" => $datos]);
+        return $this->respondWithData(["status code:" => 200, "ok" => $datos]);
     }
 }
